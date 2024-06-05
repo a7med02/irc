b6dbcc3a69e01696c88d7524ae7c6642c4a41fef
Ahmed Bouregba <abouregb@e2r2p2.1337.ma>
2024-06-05 16:05:25 +0100

    ----42----

--- bot/main.cpp
+++ bot/main.cpp
@@ -9,7 +9,6 @@ int main(int ac, char **av)
     Bot bot(av[2]);
     std::string response = "";
     std::string clinetName = "";
-    size_t contentLength = 0;
     while (true)
     {
         int ret = poll(bot.fds.data(), bot.fds.size(), -1);
@@ -18,26 +17,15 @@ int main(int ac, char **av)
             return 1;
         }
         if (bot.fds[0].revents & POLLIN){
-            std::cout << "0" << std::endl;
             clinetName = bot.receiveMessageFromServer(bot.fds[0].fd);
-            if (clinetName != ""){
-                bot.sendToApi(bot.fds);
-                bot.fds[1].events = POLLIN;
+            if (!clinetName.empty()){
+                response = bot.sendAndRecivFromApi();
+                if (bot.wrongFormat)
+                    response = "wrong format try to ask like :give me an advice";
+                bot.sendMessageToServer(bot.fds[0].fd, response, clinetName);
+                clinetName = "";
             }
-        }
-        if (bot.fds[1].revents & POLLIN){
-            std::cout << "1" << std::endl;
-            while (response.size() <= contentLength ){
-                contentLength += bot.receiveFromApi(bot.fds, response);
-                if (response != "" && response.size() > contentLength){
-                    std::cout << contentLength << std::endl;
-                    std::cout << response << std::endl;
-                    exit(-1);
-                    bot.sendMessageToServer(bot.fds[0].fd, response, clinetName);
-                        bot.fds[1].events = POLLOUT;
-                    }
-            }
-        }
-        }
-        return 0;
+        }    
     }
+    return 0;
+}
